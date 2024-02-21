d3a5b0aa3e0a71ee8cde69849cf3159a5d44b87a
MehmetPekerr <99880276+MehmetPekerr@users.noreply.github.com>
2024-02-21 22:31:20 +0300

    Add files via upload

---
+++ html projelerim/diger1/html dosyası.sql
@@ -0,0 +1,12 @@
+<form>
+    <a href="Anasayfa.html" > Anasayfa</a><br>
+    Adı:<br>
+    <input type="text" name="ad"><br>
+    E-Posta:<br>
+    <input type="text" name="E-Posta"><br>
+    Tel:<br>
+    <input type="text" name="Tel"><br>
+    Açıklama:<br>
+    <input type="text" name="Açıklama"><br>
+    <input type="button" value="Gönder">
+    </form>
