250184ffe05f3703d44727fe4982e723fc07d168
MehmetPekerr <99880276+MehmetPekerr@users.noreply.github.com>
2024-04-03 23:58:49 +0300

    Add files via upload

---
+++ Programming Languages/BagliListeElemani.java
@@ -0,0 +1,11 @@
+package Bagli_Sirala;
+
+public class BagliListeElemani {
+    int veri;
+    int adres;
+
+    public BagliListeElemani(int veri, int adres) {
+        this.veri = veri;
+        this.adres = adres;
+    }
+}
